002f4922b4943d8ea17396e11aefcb1110db64dc
William Glass <willg.glass94@gmail.com>
2023-11-01 19:01:56 -0800

    made outline for each three card type.

---
+++ threeCardPokerProb.py
@@ -0,0 +1,30 @@
+suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
+ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
+
+
+def isStraightFlush(hand):
+    # Implement logic to check for a straight
+    pass
+
+def isThreeOfAKind(hand):
+    # Implement logic to check for three of a kind
+    pass
+
+def isStraight(hand):
+    # Implement logic to check for a straight
+    pass
+
+def isFlush(hand):
+    # Implement logic to check for a flush
+    pass
+
+def isPair(hand):
+    # Implement logic to check for a pair
+    pass
+
+def isHighCard(hand):
+    # Implement logic to check for a pair
+    pass
+
+
+
