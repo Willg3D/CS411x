62f897392e54dbd7fe70a22b6464c557128a1c48
William Glass <willg.glass94@gmail.com>
2023-11-17 18:26:15 -0900

    can plot random points

--- HW5/quickhull.py
+++ HW5/quickhull.py
@@ -3,3 +3,18 @@
 # HW 5
 # 2023-11-20
 
+import matplotlib.pyplot as plt
+import numpy as np
+
+def plotRandomPoints(numPoints):
+    #np.random.seed(0)
+    points = np.random.rand(numPoints, 2)
+    
+    plt.scatter(points[:, 0], points[:, 1], color='blue')
+    plt.title(f'Random Generated Points - {numPoints} points')
+    plt.xlabel('X-axis')
+    plt.ylabel('Y-axis')
+    plt.show()
+
+numPoints = 20
+plotRandomPoints(numPoints)
