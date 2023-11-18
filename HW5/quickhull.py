e026bac781e0418633dc658eb574993ad1a90c43
William Glass <willg.glass94@gmail.com>
2023-11-17 18:45:27 -0900

    dialog box for integers made

--- HW5/quickhull.py
+++ HW5/quickhull.py
@@ -3,12 +3,14 @@
 # HW 5
 # 2023-11-20
 
-import matplotlib.pyplot as plt
-import numpy as np
+import matplotlib.pyplot as plt # used for ploting points
+from tkinter.simpledialog import askinteger  # askinteger from tkinter for dialog box
+
+import numpy as np # used for generating random points
 
 def plotRandomPoints(numPoints):
     #np.random.seed(0)
-    points = np.random.rand(numPoints, 2)
+    points = np.random.rand(numPoints, 2) # generates points (x,y) from 0 to 1
     
     plt.scatter(points[:, 0], points[:, 1], color='blue')
     plt.title(f'Random Generated Points - {numPoints} points')
@@ -16,5 +18,10 @@ def plotRandomPoints(numPoints):
     plt.ylabel('Y-axis')
     plt.show()
 
-numPoints = 20
-plotRandomPoints(numPoints)
+while(True):
+    numPoints = askinteger("Input", "Enter the number of points (Positive Ints Only):")
+    if numPoints is None:
+        break
+    if numPoints > 0:
+        plotRandomPoints(numPoints)
+        break
